db3a15c951b07bf8a76bf4e7dec02c61d0c92b34
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-01-07 14:44:48 -0300

    copy

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ btn.onclick = () => {
   document.body.classList.toggle("dark");
   document.body.classList.toggle("light");
 };
+
+const newPostBtn = document.getElementById("newPostBtnInput");
+const modalElement = document.getElementById("postModal");
+
+newPostBtn.addEventListener("click", () => {
+  const modal = new bootstrap.Modal(modalElement);
+  modal.show();
+  setTimeout(() => {
+    document.getElementById("writePostInput").focus();
+  }, 300);
+});
